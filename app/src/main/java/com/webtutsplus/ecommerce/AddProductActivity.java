a0c24b255914e197841e87acbba7e3561c77aa84
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2021-01-05 14:56:07 +0530

    Added Spinners for Category ID

--- app/src/main/java/com/webtutsplus/ecommerce/AddProductActivity.java
+++ app/src/main/java/com/webtutsplus/ecommerce/AddProductActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import okhttp3.ResponseBody;
@@ -26,6 +28,16 @@ public class AddProductActivity extends AppCompatActivity {
         etImageURL = findViewById(R.id.etImageURL1);
         etPrice = findViewById(R.id.etPrice1);
         etDescription = findViewById(R.id.etDescription1);
+
+        Spinner spinner = (Spinner) findViewById(R.id.etCategoryId1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
 
         findViewById(R.id.btnAddProduct).setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/webtutsplus/ecommerce/Product.java
+++ app/src/main/java/com/webtutsplus/ecommerce/Product.java
@@ -8,6 +8,18 @@ public class Product {
     private String description;
     private long categoryId;
 
+
+    public Product(long id, String name, String image, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.imageURL = image;
+        this.price = price;
+        this.description = description;
+    }
+
+
+
+
     public Product(long id, String name, String image, double price, String description, long categoryId) {
         this.id = id;
         this.name = name;
