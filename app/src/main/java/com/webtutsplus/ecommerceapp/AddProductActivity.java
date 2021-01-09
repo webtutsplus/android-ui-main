7ff0881b1ea1952b98a91fa89b4bdcc0b385b89f
Harsh <harsh.k9815@gmail.com>
2021-01-09 11:17:22 +0530

    Implemented Image Upload Functionality

--- app/src/main/java/com/webtutsplus/ecommerceapp/AddProductActivity.java
+++ app/src/main/java/com/webtutsplus/ecommerceapp/AddProductActivity.java
@@ -1,17 +1,32 @@
 package com.webtutsplus.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,6 +37,14 @@ public class AddProductActivity extends AppCompatActivity {
     private EditText etId, etName, etImageURL, etPrice, etDescription;
     private Spinner spinner;
     private List<Category> categories;
+
+    private static final int PICK_IMAGE_REQUEST = 9544;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +89,27 @@ public class AddProductActivity extends AppCompatActivity {
         });
 
 
+        findViewById(R.id.checkUpload1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(((CheckBox)v).isChecked()) {
+                    etImageURL.setText("Click to upload");
+                    etImageURL.setClickable(true);
+                    etImageURL.setInputType(InputType.TYPE_NULL);
+                    etImageURL.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            pick();
+                        }
+                    });
+                }
+                else {
+                    etImageURL.setInputType(InputType.TYPE_CLASS_TEXT);
+                    etImageURL.getText().clear();
+                    etImageURL.setClickable(false);
+                }
+            }
+        });
 
 
         findViewById(R.id.btnAddProduct).setOnClickListener(new View.OnClickListener() {
@@ -113,5 +157,65 @@ public class AddProductActivity extends AppCompatActivity {
         etDescription.getText().clear();
     }
 
+    public void pick() {
+        verifyStoragePermissions(AddProductActivity.this);
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Open Gallery"), PICK_IMAGE_REQUEST);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImage = data.getData();
+                String[] imageProjection = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(selectedImage, imageProjection, null, null, null);
+                if(cursor != null) {
+                    cursor.moveToFirst();
+                    int indexImage = cursor.getColumnIndex(imageProjection[0]);
+                    String part_image = cursor.getString(indexImage);
+                    File imageFile = new File(part_image);
+                    RequestBody reqBody = RequestBody.create(MediaType.parse("multipart/form-file"), imageFile);
+                    MultipartBody.Part partImage = MultipartBody.Part.createFormData("file", imageFile.getName(), reqBody);
+                    API api = RetrofitClient.getInstance().getAPI();
+                    Call<ResponseBody> upload = api.uploadImage(partImage);
+                    upload.enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                            if(response.isSuccessful()) {
+                                Toast.makeText(AddProductActivity.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
+                                try {
+                                    etImageURL.setText(response.body().string());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseBody> call, Throwable t) {
+                            Toast.makeText(AddProductActivity.this, "Request failed", Toast.LENGTH_SHORT).show();
+                            Log.i("Debug", t.getMessage());
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 }
